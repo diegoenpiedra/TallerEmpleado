969a16f0cd7cd570364f41ccdb3610e8c6c0209b
degoenpiedra <leche_o.o@hotmail.com>
2015-04-23 20:57:47 -0300

    actualizacion

--- Empleado/src/com/theopentutorials/jdbc/main/VentanaPrincipal.java
+++ Empleado/src/com/theopentutorials/jdbc/main/VentanaPrincipal.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package com.theopentutorials.jdbc.main;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
  *
  * @author diego
  */
-public class VentanaPrincipal {
+public class VentanaPrincipal extends JFrame {
+    
+    
+    public VentanaPrincipal(){
+        super("Ventana Empleado");
+        this.setSize(400,400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+        //pene
+    }
+    
     
 }
